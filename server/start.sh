a7464c41b376945b2c124a18307e1bcedf0ee921
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 14:21:54 +0100

    Auto select java executable with OS type

--- server/start.sh
+++ server/start.sh
@@ -4,11 +4,21 @@
 # Server JAR file
 serverJarPath="server.jar"
 # Path to Java
-javaExecutable="\"C:/Program Files/Java/jre1.8.0_341/bin/java.exe\""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
+		javaExecutable="java"
+elif [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
+		javaExecutable="java"
+elif [[ "$OSTYPE" == "cygwin" ]]; then # POSIX compatibility layer and Linux environment emulation for Windows
+		javaExecutable="java"
+elif [[ "$OSTYPE" == "msys" ]]; then # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+		javaExecutable="\"C:/Program Files/Java/jre1.8.0_341/bin/java.exe\""
+else # Unknown.
+	javaExecutable="java"
+fi
 # Java arguments
 javaArguments="-jar -Xms1G -Xmx4G"
 # Server JAR arguments
-jarArguments="nogui"
+jarArguments="--nogui"
 # Reboot server when stoped or crash
 looping=true
 # Wait before server start
